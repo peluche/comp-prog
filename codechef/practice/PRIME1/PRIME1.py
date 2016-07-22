e5a45b149a9c4e4b52fb0bc5f6d8c661e2610664
peluche <me@peluche.im>
2016-07-22 21:31:47 +0200

    faster solution for https://www.codechef.com/problems/PRIME1

--- codechef/practice/PRIME1/PRIME1.py
+++ codechef/practice/PRIME1/PRIME1.py
@@ -20,8 +20,25 @@ def is_prime(n, primes):
         if n % p == 0 and n != p: return False
     return True
 
-def solve(n, m, primes):
+def solve2(n, m, primes):
+    '''
+    Dummy version, test if each num in the range is prime
+    this is fast enough, and simple, but slover than the segmented sieve.
+    '''
     return '\n'.join((str(x) for x in range(max(2, n), m + 1) if is_prime(x, primes)))
+
+def solve(n, m, primes):
+    ''' Segmented sieve '''
+    n, m = max(2, n), max(2, m)
+    stop = math.sqrt(m) + 1
+    nums = [True] * (m + 1 - n)
+    for p in primes:
+        if p > stop: break
+        if n % p == 0: start = n
+        else: start = n + p - n % p
+        for j in range(max(p * 2, start) - n, m + 1 - n, p):
+            nums[j] = False
+    return '\n'.join(str(x) for x, y in zip(range(n, m + 1), nums) if y)
 
 if __name__ == '__main__':
     for _ in range(int(raw_input())):
